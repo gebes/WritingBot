5506a8d07fef50fcdfb2dc38ac8a9f217cd6a48a
Gebes <35232234+GebesPlays@users.noreply.github.com>
2020-04-22 21:44:10 +0200

    Created Project

--- src/eu/gebes/writingBot/Main.java
+++ src/eu/gebes/writingBot/Main.java
@@ -1,10 +1,13 @@
 package eu.gebes.writingBot;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        
+        System.out.println("Enter a string to write: ");
+        final String stuffToWrite = new Scanner(System.in).next();
 
     }
 }
