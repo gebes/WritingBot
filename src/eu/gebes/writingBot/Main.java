e2eef422c10fcc6c8dea3f1a4614589a76251a83
Gebes <35232234+GebesPlays@users.noreply.github.com>
2020-04-22 21:47:20 +0200

    Added Timer

--- src/eu/gebes/writingBot/Main.java
+++ src/eu/gebes/writingBot/Main.java
@@ -6,8 +6,40 @@ public class Main {
 
     public static void main(String[] args) {
 
+        WritingBot writingBot = new WritingBot();
+
+        writingBot.exec();
+
+    }
+
+
+}
+
+class WritingBot {
+
+    public void exec() {
         System.out.println("Enter a string to write: ");
         final String stuffToWrite = new Scanner(System.in).next();
 
+        timer(3);
+
     }
+
+    private static void timer(int duration) {
+
+        for (int i = duration; i > 0; i--) {
+            System.out.println("Entering input in " + i + " seconds");
+            sleep(1);
+        }
+
+    }
+
+    private static void sleep(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
